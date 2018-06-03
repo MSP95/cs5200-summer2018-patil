1d9ede0717343aa59e2e8c2b91bb66b0a9da828d
MSP95 <patilmanish1910@gmail.com>
2018-06-03 10:59:12 -0400

    Integrating Spring Boot with MySQL

--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.northeastern.cs5200.controllers.hello;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HelloRepository
-
- extends JpaRepository<HelloObject, Integer> {
-
-}
